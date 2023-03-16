72f24aab0d537a165d94d384708fa1078b9e7936
Jon Charette <jon@harness.io>
2023-03-16 16:12:14 -0400

    adding more outputs to gke

--- gcp/gke/outputs.tf
+++ gcp/gke/outputs.tf
@@ -7,3 +7,20 @@ output "endpoint" {
     description = "Endpoint for cluster"
     value = google_container_cluster.primary.endpoint
 }
+
+output "cluster_endpoint" {
+ value = google_container_cluster.primary.endpoint
+}
+
+output "client_certificate" {
+ value = google_container_cluster.primary.master_auth.0.client_certificate
+}
+
+output "client_key" {
+ value = google_container_cluster.primary.master_auth.0.client_key
+ sensitive = true
+}
+
+output "cluster_ca_certificate" {
+ value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}

c541f64cbe306f2784e3b7036a7b061ea51c2a94
Jon Charette <jon@harness.io>
2023-03-17 10:34:20 -0400

    testing new k8s auth

--- k8s/secret/main.tf
+++ k8s/secret/main.tf
@@ -2,14 +2,16 @@ provider "google" {
     project = var.gcp_project_id
     region = var.gcp_region
 }
+
+
+data "google_client_config" "provider" {}
+
 provider "kubernetes" {
-    
-    username = ""
-    password = "" 
+    token = data.google_client_config.provider.access_token
 
     host = var.cluster_endpoint
-    client_certificate = base64decode(var.client_certificate)
-    client_key = base64decode(var.client_key)
+#    client_certificate = base64decode(var.client_certificate)
+#    client_key = base64decode(var.client_key)
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
 }
 resource "kubernetes_secret" "this" {

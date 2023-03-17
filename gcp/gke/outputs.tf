10c111af47d2ae960820ba862dc08a97a7e181b0
Jon Charette <jon@harness.io>
2023-03-17 10:07:52 -0400

    merge fix

--- gcp/gke/outputs.tf
+++ gcp/gke/outputs.tf
@@ -20,7 +20,6 @@ output "cluster_ca_certificate" {
 }
 
 output "self_link" {
-<<<<<<< Updated upstream
     value = google_container_cluster.primary.self_link
 }
 
@@ -29,23 +28,3 @@ output "endpoint" {
     value = google_container_cluster.primary.endpoint
 }
 
-output "cluster_endpoint" {
- value = google_container_cluster.primary.endpoint
-}
-
-output "client_certificate" {
- value = google_container_cluster.primary.master_auth.0.client_certificate
-}
-
-output "client_key" {
- value = google_container_cluster.primary.master_auth.0.client_key
- sensitive = true
-}
-
-output "cluster_ca_certificate" {
- value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-}
-=======
- value = google_container_cluster.primary.self_link
-}
->>>>>>> Stashed changes

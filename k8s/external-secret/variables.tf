58353ff4d2b2a792e1b4fac63385b45ae72136f9
Jon Charette <jon@harness.io>
2023-07-02 20:39:59 -0400

    adding external-secret templating module

---
+++ k8s/external-secret/variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "secretName" {
+  type = string
+}
+
+variable "secretNamespace" {
+  type = string
+}
+
+variable "clusterSecretStore" {
+  type = string
+}
+
+variable "secretType" {
+  type = string
+}
+
+variable "secretTemplate" {
+  type = map(string)
+}
+
+variable "secretDataRef" {
+  type = map(string)
+}
